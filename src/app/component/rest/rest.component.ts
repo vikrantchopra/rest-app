b0ff278f90fdff475f993da3541d1b48471df47f
Vikrant Chopra <Vikrant.Chopra@hyland.com>
2020-06-17 20:18:34 +0530

    tabs dynamic

--- src/app/component/rest/rest.component.ts
+++ src/app/component/rest/rest.component.ts
@@ -10,20 +10,42 @@ import { RestService } from '../../service/rest.service';
 export class RestComponent implements OnInit {
 
   restdata: any[];
-  
+
+  tabHeaders = [];
+
 
   constructor(private restService: RestService) { }
 
   ngOnInit(): void {
     this.getAll()
     //this.getAllRecords();
+
+    //this.restdata = this.getRecords();
+    //console.log("Length: " + this.restdata.length);
   }
 
   getAll() {
     this.restService.getAll().subscribe(result => {
       console.log(result)
       this.restdata = result
+
+      this.setTabHeaders(result);
     });
+  }
+
+
+
+  setTabHeaders(result) {
+    var i = 0;
+    var tabset = new Set();
+
+    result.forEach(element => {
+      console.log("Tabs " + element.testID);
+      tabset.add(element.testID);
+
+    });
+    this.tabHeaders = Array.from(tabset);
+    console.log("The first tab: " + this.tabHeaders[0]);
   }
 
   getAllRecords() {
